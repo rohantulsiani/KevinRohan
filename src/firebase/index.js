e1e06d101fc2bfb4adb8abd1357fc7b1dcb3fa6e
Rohan Tulsiani <rohantulsiani@gmail.com>
2017-11-20 18:58:29 -0800

    reverse array bug fix

--- src/firebase/index.js
+++ src/firebase/index.js
@@ -246,7 +246,7 @@ export const addEntity = (user, uid, entityType, options, owner, subject, timeLi
 		uid, entityType, options, owner, subject, timeLimit, anonymous, category, details, tags, timeCreatedAt
 	}
 	
-	pushToFollowers(user, "CreatePost", {owner, subject, entityType});
+	if(!anonymous) { pushToFollowers(user, "CreatePost", {owner, subject, entityType}); }
 	return firebase.database().ref('entities/').push(toPush)
 }
 
